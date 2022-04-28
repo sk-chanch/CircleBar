9bfd55c09d0c828169c2118c90636b97351c30bb
skipp <chanchanakoedtho@gmail.com>
2022-04-28 21:53:20 +0700

    Fixed swift package

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CircleBar",
+    platforms: [.iOS(.v10)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CircleBar",
+            targets: ["CircleBar"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMinor(from: "6.5.0")),
+        .package(url: "https://github.com/sk-chanch/RxViewController.git", .upToNextMinor(from: "2.2.0"))
+        
+        
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "CircleBar",
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "RxViewController", package: "RxViewController")
+            ],
+            path: "CircleBar/Classes")
+    ]
+)
